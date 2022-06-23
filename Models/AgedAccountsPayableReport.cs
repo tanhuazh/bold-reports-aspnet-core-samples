422f85412c8f7564fd5b47ba4e0317ecb75063bc
Michael Tan <tanm@fcc.ca>
2022-06-23 08:21:39 -0400

    add datasource and data

---
+++ Models/AgedAccountsPayableReport.cs
@@ -0,0 +1,17 @@
+﻿
+namespace AgExpert.Accounting.Models.DataObjects
+{
+    public class AgedAccountsPayableReport
+    {
+        public string ContactId { get; set; }
+        public string BusinessName { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string Supplier { get; set; }
+        public decimal PayableBalance { get; set; }
+        public decimal ThirtyDaysTotal { get; set; }
+        public decimal SixtyDaysTotal { get; set; }
+        public decimal NinetyDaysTotal { get; set; }
+        public decimal PlusNinetyDaysTotal { get; set; }
+    }
+}
